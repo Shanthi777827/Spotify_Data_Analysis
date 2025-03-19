b2b537471746e1b3897437dc71cc6bf76ca9d25a
Shanthi Nirmala A <nirmalashanthi04@gmail.com>
2025-03-19 18:03:36 +0530

    Delete #Basic Python code.py

--- #Basic Python code.py
+++
@@ -1,8 +0,0 @@
-#Basic Python code
-a=int(input('enter the value of a:'))
-b=int(input('enter the value of b:'))
-c=int(input('enter the value of c:'))  
-equation=3*(a**2)+b-(2*c)
-print(equation)
-
-    
